400983c02ceb0bd8f9e28d7eb91e13e06d952362
Exentio <sendnudes@exentio.sexy>
2019-12-17 00:06:01 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+Python wrapper for nHentai APIs
+===
+
+This wrapper uses the official (and poorly documented) nHentai APIs to get info about the content of your favorite website.  
+It's still a WIP, so be careful.
+
+### Currently available methods
+`Nhentai().all(page_num=1)`: Returns all the doujin on nHentai's homepage; to get the other pages, use the `page_num` parameter.  
+`Nhentai().latest()`: Returns info about the latest uploaded doujin.  
+`Nhentai().latest_id()`: Returns the ID of the latest uploaded doujin. Useful for random generators.  
+`Nhentai().query_search(user_query, page_num=1)`: Performs a search. Put your keywords in `user_query`; to get the other pages, use the `page_num` parameter.  
+`Nhentai().book_info(book_id)`: Gets all the info about a magic number of your choice, like `177013`.  
+
+### TODO
++ Search by tag. nHentai APIs use tag IDs instead of words, so I have to find a way to find the ID that matches a certain tag. I think I'll go with scraping them from the `/tags` page.  
++ Pages and covers.  
++ Selective data. Like getting all the tags, the group name, the parody, and stuff like this.  
++ Look for bugs. I did some basic debugging but it was pretty late when I wrote this.  
+
+### Resources used
++ [https://github.com/NHMoeDev/NHentai-android/issues/27](https://github.com/NHMoeDev/NHentai-android/issues/27)  
++ [https://edgyboi2414.github.io/nhentai-api](https://edgyboi2414.github.io/nhentai-api)
++ [https://github.com/RicterZ/nhentai](https://github.com/RicterZ/nhentai)
---
+++ nhentai.py
@@ -0,0 +1,55 @@
+import json
+import requests
+import urllib.parse
+
+class Nhentai():
+    BASE_URL = "https://nhentai.net/api/"
+
+    @classmethod
+    def make_request(cls, url):
+        response = requests.get(url)
+        if not response:
+            return json.loads('{"error": true}')
+        return json.loads(response.text)
+
+    def all(self, page_num=1):
+        url = self.BASE_URL + "galleries/all"
+
+        parsed_response = self.make_request(url)
+
+        if not isinstance(page_num, int):
+            return json.loads('{"error": true}')
+
+        max_pages = parsed_response["num_pages"]
+        if page_num > 1 and page_num <= max_pages:
+            url = url + "?page=" + str(page_num)
+            parsed_response = self.make_request(url)
+
+        return parsed_response
+
+    def latest(self):
+        all_parsed_response = self.all()
+        return all_parsed_response["result"][0]
+
+    def latest_id(self):
+        return self.latest()["id"]
+
+    def query_search(self, user_query, page_num=1):
+        query = urllib.parse.quote(user_query, safe='')
+        url = self.BASE_URL + "galleries/search?query=" + query
+
+        parsed_response = self.make_request(url)
+
+        if not isinstance(page_num, int):
+            return json.loads('{"error": true}')
+
+        max_pages = parsed_response["num_pages"]
+        if page_num > 1 and page_num <= max_pages:
+            url = url + "?page=" + page_num
+            parsed_response = self.make_request(url)
+
+        return parsed_response
+
+    def book_info(self, book_id):
+        url = self.BASE_URL + "gallery/" + book_id
+        return self.make_request(url)
